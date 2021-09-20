c6d4e803175790cd62d370a9ed50f65ec8a185ce
nick0916 <nickliao2013@gmail.com>
2021-09-20 16:25:31 +0800

    feat: create 架構

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production : false,
+    title : 'NCKUHUB DEV',
+    appEndPoint : 'http://localhost:8080',
+    logoutUrl: '/login',
+    resetParamFlag: true,
+    errorRedirectFlag: false,
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
